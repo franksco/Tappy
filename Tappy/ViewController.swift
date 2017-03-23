5fa982734eb583ef799950ccdb4f11fd1b178f3d
franksco <franksco39@gmail.com>
2017-03-22 17:01:15 -0700

    It will add IBOutlet's and IBActions for labels and buttons.

--- Tappy/ViewController.swift
+++ Tappy/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var tapMeButton: UIButton!
+    @IBOutlet var startGameButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func TapMe(_ sender: Any) {
+        
+        
+    }
+    @IBAction func StartGame(_ sender: Any) {
+        
+        
+    }
 
 }
 
